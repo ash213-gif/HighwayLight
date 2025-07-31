7063632ca56abe5851f841a05f141b436a3ae13f
Ashish <ashish789pa@gmail.com>
2025-07-31 14:28:10 +0530

    check backened deploye render

--- Backened/src/Routes/routes.ts
+++ Backened/src/Routes/routes.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 const router = express.Router();
+import { Request,Response } from 'express'
 import {createUser ,login} from '../Controller/user'
 
 router.post('/createUser',createUser  )
 router.post('/login',login  )
+router.get('/', (req: Request ,res : Response)=>{
+    res.json('thish hdiuhdi db my route ashis pancjal')
+} )
 
 export default router;
